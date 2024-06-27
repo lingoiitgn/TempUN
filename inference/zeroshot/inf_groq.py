2464cac6ce25cbcafab50b25cfe3e5815fbf74db
Dishant Patel <102381069+dishantpatel-GH@users.noreply.github.com>
2024-06-27 16:55:05 +0530

    Update inf_groq.py

--- inference/zeroshot/inf_groq.py
+++ inference/zeroshot/inf_groq.py
@@ -1,1 +1,90 @@
+from groq import Groq
+client = Groq(api_key="")        # add api_key from groq_console
 
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Inferencing using vLLM')
+parser.add_argument('--seed', type=int, default=0, help='Seed for reproducibility')
+parser.add_argument('--base-dir', type=str, default='/path_to_csvs', help='Path to the csvs')
+parser.add_argument('--out-dir', type=str, default='/path_to_results', help='Path to the output directory')
+parser.add_argument('--year', type=int, default=1947, help='Year from which the inference is to be done')
+
+args = parser.parse_args()
+
+# Defining the seed
+SEED = 1
+
+FOLDER_PATH = args.base_dir
+last_folder = FOLDER_PATH.split("/")[-1]
+results_folder=args.out_dir
+# Defining a prompt suffix
+SUFFIX = '. Provide only the correct option, without explaination.'
+
+import torch
+
+torch.cuda.empty_cache()
+
+option_regex = re.compile(r"\([a-zA-Z]\)")
+yes_no_regex = re.compile(r"\b(yes|no)\b", re.IGNORECASE)
+
+ignore_folders = [""]
+import os
+import pandas as pd
+
+for i in os.listdir(FOLDER_PATH):
+    if i in ignore_folders:
+        continue
+    for j in os.listdir(os.path.join(FOLDER_PATH, i)):
+        csv_path = os.path.join(FOLDER_PATH, i, j)
+        # print(csv_path)
+        if not str(csv_path).endswith(".csv") or str(args.year) not in csv_path:
+            continue
+        print(f"-------Processing file: {os.path.join(i,j)}-------")
+        prompts_df = pd.read_csv(csv_path)
+        prompts_df['modified_prompt'] = prompts_df['query'].apply(lambda x: x + SUFFIX)
+        prompts = prompts_df['modified_prompt'].tolist()
+
+        results = []
+        for prompt in prompts:
+          try:
+            # Create the chat completion
+            chat_completion = client.chat.completions.create(
+              messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+              ],
+              model="llama3-70b-8192",
+              temperature=0.01,
+              max_tokens=50,
+              top_p=0.95,
+            )
+
+            # Store the result in the 'results' list
+            results.append(chat_completion.choices[0].message.content)
+
+          except Exception as e:
+            results.append("")  # Append empty string for error cases
+
+        prompts_df['generated_text'] = results
+      
+        extracted_ans = []
+
+        for ans in results:
+            option_match = option_regex.findall(ans)
+            yes_no_match = yes_no_regex.findall(ans)
+            if option_match:
+                extracted_ans.append(option_match[0].strip("()"))
+            elif yes_no_match:
+                extracted_ans.append(yes_no_match[-1].capitalize())
+            else:
+                extracted_ans.append("")
+
+      
+        prompts_df['extracted_answer'] = extracted_ans
+        os.makedirs(os.path.join(results_folder, i), exist_ok=True)
+        res_path = os.path.join(results_folder, i, j)
+        print(f"Saving to {res_path}")
+        prompts_df.to_csv(res_path)
