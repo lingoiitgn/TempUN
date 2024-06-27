5822712193f63caf628ab3dc1c60fa444559e7b6
Dishant Patel <102381069+dishantpatel-GH@users.noreply.github.com>
2024-06-27 12:22:19 +0530

    Update run.py

--- inference/after_FT/inf_transformers/run.py
+++ inference/after_FT/inf_transformers/run.py
@@ -1,1 +1,37 @@
+import os
+os.environ['HF_TOKEN'] = ''    # add HF access token
 
+import argparse
+import time
+
+parser = argparse.ArgumentParser(description='Temporal Inference')
+parser.add_argument('--cuda', type=str, default="0", help='Cuda device')
+parser.add_argument('--models', type=str, required=True, help='Model name') 
+parser.add_argument('--hub-model-name', type=str, default='google/gemma-1.1-7b-it', help='Model name')
+parser.add_argument('--base-dir', type=str, default='/path_to_csvs', help='Path to the csvs') 
+parser.add_argument('--out-dir', type=str, default='/path_to_results', help='Path to the output directory')
+parser.add_argument('--year', type=int, default=1947, help='Year to run inference (Runs from this year)')
+
+args = parser.parse_args()
+
+years_folder = os.listdir(args.models)
+years_folder.sort()
+for year_ in years_folder:
+    model_name = args.hub_model_name.split("/")[0]
+    year = year_.split("-")[0]
+    if int(year) < args.year:
+        continue
+    print(f"Year: {year}")
+    chkpoints = os.listdir(f"{args.models}/{year_}")
+    chkpoints.sort()
+    checkpoint = chkpoints[0] if "checkpoint" in chkpoints[0] else chkpoints[1]
+    checkpoint = f"{args.models}/{year_}/{checkpoint}"
+    print(f"Checkpoint: {checkpoint}")
+    # Convert to original model
+    os.system(f"CUDA_VISIBLE_DEVICES={args.cuda} python /path_to_lora_to_original.py/lora_to_original.py --model {args.hub_model_name} --adapter {checkpoint} --output /path_to_save_models_from_lora_to_original/{model_name}")
+
+    # Run inference
+    print(f"Running inference for {year}")
+    os.system(f"CUDA_VISIBLE_DEVICES={args.cuda} python /path_to_code4.py/code4.py --model /path_to_save_models_from_lora_to_original/{model_name} --base-dir {args.base_dir} --out-dir {args.out_dir}/{model_name}/{year} --year {year} --hub-model-name {args.hub_model_name}")
+
+    time.sleep(3)
